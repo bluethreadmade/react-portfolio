a2a0ede393c067128651337d504079cd19cbbb3a
Megan McNeill <mcneim2@gmail.com>
2024-08-03 15:05:30 -0400

    added footer and placeholder links

---
+++ src/components/FooterLinks.jsx
@@ -0,0 +1,22 @@
+// Bringing in the required import from 'react-router-dom'
+import { Link } from "react-router-dom";
+import Footer from "./UI/Footer";
+
+export default function FooterLinks() {
+  // The Footerlinks UI component will render each of the Link elements in the links prop
+  return (
+    <Footer
+      links={[
+        <Link key={1} className="nav-link text-light" to="#">
+          Github{" "}
+        </Link>,
+        <Link key={2} className="nav-link text-light" to="#">
+          LinkedIn
+        </Link>,
+        <Link key={2} className="nav-link text-light" to="#">
+          Artisans
+        </Link>,
+      ]}
+    />
+  );
+}
