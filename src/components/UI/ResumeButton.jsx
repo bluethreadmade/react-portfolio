acd02cdad8edf1474301e1b84d931618fb5e9f2d
Megan McNeill <mcneim2@gmail.com>
2024-08-07 22:02:54 -0400

    contact page email and message validations

--- src/components/UI/ResumeButton.jsx
+++ src/components/UI/ResumeButton.jsx
@@ -1,6 +1,5 @@
 export default function ResumeButton() {
     return (
-        <a href="./Megan McNeill Resume.pdf" class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none " download>Download Resume</a>
+        <a href="./Megan McNeill Resume.pdf" class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent" download>Download Resume</a>
     )
-
 }
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,5 +1,13 @@
+import { useState } from 'react';
+
 // This is a static page with a contact form
 export default function ContactPage() {
+    const [ name, setName ] = useState('');
+    const [ email, setEmail ] = useState('');
+    const [ message, setMessage ] = useState('');
+    const [ messageActive, setMessageActive ] = useState(true);
+    const [ isEmailValid, setIsEmailValid ] = useState();
+
     return (
         <div>
         <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
@@ -7,47 +15,27 @@ export default function ContactPage() {
                 <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
                     Contact Me!
                 </h2>
-                <p className="mt-2 text-lg leading-8 text-gray-600">
-                    Aute magna irure deserunt veniam aliqua magna enim
-                    voluptate.
-                </p>
             </div>
             <form
                 action="#"
-                method="POST"
+                method=""
                 className="mx-auto mt-16 max-w-xl sm:mt-20"
             >
                 <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
                     <div>
                         <label
-                            htmlFor="first-name"
+                            htmlFor="name"
                             className="block text-sm font-semibold leading-6 text-gray-900"
                         >
-                            First name
+                            Name
                         </label>
                         <div className="mt-2.5">
                             <input
-                                id="first-name"
-                                name="first-name"
+                                value={name}
+                                onChange={e => setName(e.target.value)}
+                                id="name"
+                                name="name"
                                 type="text"
-                                autoComplete="given-name"
-                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            />
-                        </div>
-                    </div>
-                    <div>
-                        <label
-                            htmlFor="last-name"
-                            className="block text-sm font-semibold leading-6 text-gray-900"
-                        >
-                            Last name
-                        </label>
-                        <div className="mt-2.5">
-                            <input
-                                id="last-name"
-                                name="last-name"
-                                type="text"
-                                autoComplete="family-name"
                                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
                         </div>
@@ -61,12 +49,17 @@ export default function ContactPage() {
                         </label>
                         <div className="mt-2.5">
                             <input
+                                value={email}
+                                onChange={e => {setEmail(e.target.value); setIsEmailValid(e.target.validity.valid)}}
+                                //onMouseLeave={ () => setIsEmailValid(e.target.validity.valid)}
                                 id="email"
                                 name="email"
                                 type="email"
                                 autoComplete="email"
                                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
+                            {!isEmailValid && <p className="text-red-500 text-sm">Enter A Valid Email</p>}
+
                         </div>
                     </div>
                     <div className="sm:col-span-2">
@@ -78,12 +71,17 @@ export default function ContactPage() {
                         </label>
                         <div className="mt-2.5">
                             <textarea
+                                value={message}
+                                onChange={e => setMessage(e.target.value)}
+                                onMouseEnter={ () => setMessageActive(true)}
+                                onMouseLeave={ () => setMessageActive(false)}
                                 id="message"
                                 name="message"
                                 rows={4}
                                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 defaultValue={''}
                             />
+                            {!messageActive && !message && <p className="text-red-500 text-sm">Message Required</p>}
                         </div>
                     </div>
                 </div>
