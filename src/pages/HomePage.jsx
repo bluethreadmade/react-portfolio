34bff4693fa2d3dde783dfcfbaeb133ed7fae071
Megan McNeill <mcneim2@gmail.com>
2024-08-15 11:15:17 -0400

    added paragraph breaks and indents

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,16 +10,12 @@ export default function HomePage() {
                     className="mb-12 w-36 rounded-full object-cover lg:w-64"
                 ></img>
                 <div>
-                    <p className="mx-16 text-lg font-medium text-gray-800">
-                        ABOUT ABOUT ABOUT Lorem ipsum dolor sit amet consectetur
-                        adipisicing elit. Repellendus tenetur maiores, dolor
-                        iusto dolorum ullam, natus deleniti blanditiis impedit
-                        suscipit sed magnam alias in, repellat expedita hic
-                        explicabo architecto soluta. About us Lorem ipsum, dolor
-                        sit amet consectetur adipisicing elit. Velit voluptate
-                        exercitationem quaerat pariatur mollitia, excepturi,
-                        voluptatem eveniet a dolor nobis ex veniam totam nostrum
-                        temporibus ad omnis nam rerum eligendi.
+                    <p className="mx-16 text-base lg:mt-24 lg:text-lg font-medium text-gray-800">
+                        <p className="mb-4 indent-10">With 14 years of hands-on experience in product design and engineering, I’ve honed my ability to create impactful solutions that enhance user experiences and drive innovation.</p> 
+                        
+                        <p className="mb-4 indent-10">Recently, I’ve expanded my skill set to include web development, where I’ve embraced the challenge of building dynamic and responsive web applications. My background in mechanical engineering and program management has instilled a deep understanding of design principles and a strategic mindset that I now apply to both digital and physical products.</p>
+
+                        <p className="mb-4 indent-10">I thrive on turning complex ideas into intuitive, user-centric designs and enjoy leveraging my technical expertise to build and refine systems, workflows, and digital interfaces. My passion for creating meaningful solutions and my commitment to continuous learning enable me to excel in crafting high-quality, innovative web solutions that align with user needs and business goals.</p>
                     </p>
                     <div className="mx-16 my-16 flex justify-center">
                         <ResumeButton />
